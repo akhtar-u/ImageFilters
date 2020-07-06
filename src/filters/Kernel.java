f01e13622675a6f6ab83f647a6167315cc59ed27
Usman Akhtar <akhtar_usman@outlook.com>
2020-07-05 21:57:32 -0400

    added more methods to Kernel class and added documentation

--- src/filters/Kernel.java
+++ src/filters/Kernel.java
@@ -1,32 +1,76 @@
 package filters;
 
 /**
- * A kernel matrix in 1-dimension for Sharpen and Gaussian
+ * A 3x3 kernel in 1-dimension for Sharpen and Gaussian
  * Blur image filters.
  */
 public class Kernel {
 
     /**
-     * The size of the kernel
+     * The type of kernel
      */
-    public final int kernelSize;
-
-    /**
-     * Initializes a kernel with size 9 (3 x 3)
-     */
-    public Kernel (){
-        kernelSize = 9;
-    }
+    public final String kernelType;
+    private double[] kernelArray;
 
     /**
      * Initializes a kernel with a {@code kernelSize} as
-     * specified by the caller.
+     * specified by the caller.     *
      *
-     * @param kernelSize the size of the kernel
-     * @throws IllegalArgumentException if {@code kernelSize}
-     * is not odd and greater than 3
+     * @param kernelType the type of kernel to initialize ("blur" or "sharpen")
+     * @throws IllegalArgumentException if {@code kernelType} is not one
+     *                                  of "blur" or "sharpen" or "edge"
      */
-    public Kernel(int kernelSize) {
-        this.kernelSize = kernelSize;
+    public Kernel(String kernelType) {
+
+        if (kernelType.toLowerCase() != "sharpen" || kernelType.toLowerCase() != "blur" || kernelType.toLowerCase() != "edge") {
+            throw new IllegalArgumentException();
+        } else if (kernelType.toLowerCase() == "blur") {
+            kernelArray = blurKernel();
+        } else if (kernelType.toLowerCase() == "sharpen") {
+            kernelArray = sharpenKernel();
+        } else if (kernelType.toLowerCase() == "edge") {
+            kernelArray = edgeKernel();
+        }
+
+        this.kernelType = kernelType;
+    }
+
+
+    /**
+     * @return the type of this {@code Kernel}
+     */
+    public String getKernelType() {
+        return kernelType;
+    }
+
+    /**
+     * @return the data of this {@code Kernel} as a 1-D array
+     */
+    public double[] getKernelArray() {
+        return kernelArray;
+    }
+
+    /**
+     * @return a 1-D array for a Gaussian blur kernel
+     */
+    private double[] blurKernel() {
+
+        return null;
+    }
+
+    /**
+     * @return a 1-D array for a sharpen kernel
+     */
+    private double[] sharpenKernel() {
+
+        return new double[]{-1d, -1d, -1d, -1d, 9d, -1d, -1d, -1d, -1d};
+    }
+
+    /**
+     *
+     * @return a 1-D array for an edge detection kernel (details of image)
+     */
+    private double[] edgeKernel() {
+        return new double[]{1/9d, 1/9d, 1/9d, 1/9d, 1/9d, 1/9d, 1/9d, 1/9d, 1/9d};
     }
 }
