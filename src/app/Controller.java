f25553740d74818ca85f677b669009dc1cdf50b9
Usman Akhtar <akhtar_usman@outlook.com>
2020-07-11 23:05:51 -0400

    added exit button functionality to menu of application

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,5 +1,6 @@
 package app;
 
+import javafx.application.Platform;
 import javafx.scene.control.*;
 import java.awt.*;
 import java.io.IOException;
@@ -15,10 +16,8 @@ public class Controller {
         alert.setContentText("Usman Akhtar © 2020");
 
         ButtonType VISIT = new ButtonType("Visit GitHub");
-
         alert.getButtonTypes().addAll(ButtonType.CLOSE, VISIT);
         alert.getButtonTypes().removeAll(ButtonType.OK, ButtonType.CLOSE);
-
 
         Optional<ButtonType> result = alert.showAndWait();
         if (result.isPresent() && result.get() == ButtonType.CLOSE) {
@@ -26,12 +25,25 @@ public class Controller {
         } else if (result.isPresent() && result.get() == VISIT) {
             try {
                 Desktop.getDesktop().browse(new URI("https://github.com/akhtar-u"));
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            } catch (URISyntaxException e1) {
+            } catch (IOException | URISyntaxException e1) {
                 e1.printStackTrace();
             }
         }
-
     }
+
+    public void exitApp(){
+        Platform.exit();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        primaryStage.setTitle("Image Editor");
+        primaryStage.setTitle("Image Filters");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.getIcons().add(new Image("file:icon.png"));
         primaryStage.show();
