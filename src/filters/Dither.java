631b05071775d3300086a9ac65858cf974558c06
Usman Akhtar <akhtar_usman@outlook.com>
2020-07-15 00:49:46 -0400

    added last two filters empty classes
    Former-commit-id: 89c500ad677af255a497f0ecab80536092c3270f

---
+++ src/filters/Dither.java
@@ -0,0 +1,21 @@
+package filters;
+
+/**
+ * A filter which uses error diffusion to
+ * reduce color space of an image
+ *
+ * @author Usman Akhtar
+ */
+public class Dither {
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/filters/Median.java
@@ -0,0 +1,9 @@
+package filters;
+
+/**
+ * A filter which removes noise from an image
+ *
+ * @author Usman Akhtar
+ */
+public class Median {
+}
