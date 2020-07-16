414eab7542212dae1e459b52dda3a77b2386dcb7
Usman Akhtar <akhtar_usman@outlook.com>
2020-07-16 17:31:14 -0400

    added dithering effect
    Former-commit-id: 4a4f4592f6df0aa70f88993537a6851116bc55db

--- src/filters/Dither.java
+++ src/filters/Dither.java
@@ -15,11 +15,54 @@ public class Dither {
             Color.YELLOW, Color.CYAN, Color.MAGENTA, Color.white};
 
     public void ditherImage(int[] imageData, int imgWidth) {
-        Color newColor;
+        Color actualColor, newColor;
+        int errR, errG, errB, alpha;
+        double red, green, blue;
 
         for (int i = 0; i < imageData.length; i++) {
+            actualColor = new Color(imageData[i]);
             newColor = findNearestColor(imageData, i);
+            alpha = (imageData[i] >> 24) & 0xff;
+
+            errR = actualColor.getRed() - newColor.getRed();
+            errG = actualColor.getGreen() - newColor.getGreen();
+            errB = actualColor.getBlue() - newColor.getBlue();
+
             imageData[i] = newColor.getRGB();
+
+            /*if (i + 1 < imgWidth){
+                red = roundRGB(FilterUtility.getPixel(imageData, i + 1, 0) + (7.0 / 16.0 * errR));
+                green = roundRGB(FilterUtility.getPixel(imageData, i + 1, 1) + (7.0 / 16.0 * errG));
+                blue = roundRGB(FilterUtility.getPixel(imageData, i + 1, 2) + (7.0 / 16.0 * errB));
+
+                imageData[i + 1] = alpha << 24 | (int) red << 16 | (int) green << 8 | (int) blue;
+            }
+
+            if (i - 1 >= 0 && i + imgWidth < imageData.length){
+                red = roundRGB(FilterUtility.getPixel(imageData, i - 1 + imgWidth, 0) + (3.0 / 16.0 * errR));
+                green = roundRGB(FilterUtility.getPixel(imageData, i - 1 + imgWidth, 1) + (3.0 / 16.0 * errG));
+                blue = roundRGB(FilterUtility.getPixel(imageData, i - 1 + imgWidth, 2) + (3.0 / 16.0 * errB));
+
+                imageData[i - 1 + imgWidth] = alpha << 24 | (int) red << 16 | (int) green << 8 | (int) blue;
+            }
+
+            if (i + imgWidth < imageData.length){
+                red = roundRGB(FilterUtility.getPixel(imageData, i + imgWidth, 0) + (5.0 / 16.0 * errR));
+                green = roundRGB(FilterUtility.getPixel(imageData, i + imgWidth, 1) + (5.0 / 16.0 * errG));
+                blue = roundRGB(FilterUtility.getPixel(imageData, i + imgWidth, 2) + (5.0 / 16.0 * errB));
+
+                imageData[i + imgWidth] = alpha << 24 | (int) red << 16 | (int) green << 8 | (int) blue;
+            }
+
+            if (i + 1 < imgWidth && i + imgWidth < imageData.length){
+                red = roundRGB(FilterUtility.getPixel(imageData, i + 1 + imgWidth, 0) + (1.0 / 16.0 * errR));
+                green = roundRGB(FilterUtility.getPixel(imageData, i + 1 + imgWidth, 1) + (1.0 / 16.0 * errG));
+                blue = roundRGB(FilterUtility.getPixel(imageData, i + 1 + imgWidth, 2) + (1.0 / 16.0 * errB));
+
+                imageData[i + 1 + imgWidth] = alpha << 24 | (int) red << 16 | (int) green << 8 | (int) blue;
+
+            }  */
+
         }
     }
 
@@ -45,5 +88,12 @@ public class Dither {
             }
         }
         return nearestColor;
+    }
+
+    private double roundRGB (double value) {
+        if (value < 0) value = 0;
+        else if (value > 255) value = 255;
+
+        return value;
     }
 }
