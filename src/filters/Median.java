de2f332dc18bb0e1edc1227e06affdf692af9877
Usman Akhtar <akhtar_usman@outlook.com>
2020-07-18 17:46:22 -0400

    copied convulation code from sharpen to median
    Former-commit-id: fbcab6248c66642d35cfbe0d00289f1c5b1123b3

--- src/filters/Median.java
+++ src/filters/Median.java
@@ -2,11 +2,54 @@ package filters;
 
 /**
  * A filter which removes noise from an image
+ * and smooths it while preserving edges
  *
  * @author Usman Akhtar
  */
 public class Median {
 
-    public void medianImage(int[] currentImageData, int imgWidth) {
+    public void medianImage(int[] imageData, int imgWidth) {
+
+        double red, green, blue;
+        int alpha;
+        int[] medianImage = new int[imageData.length];
+
+        for (int i = 0; i < imageData.length; i++) {
+
+            alpha = (imageData[i] >> 24) & 0xff;
+
+            red = FilterUtility.getPixel(imageData, i - imgWidth - 1, 0);
+            red += FilterUtility.getPixel(imageData, i - imgWidth, 0);
+            red += FilterUtility.getPixel(imageData, i - imgWidth + 1, 0);
+            red += FilterUtility.getPixel(imageData, i - 1, 0);
+            red += FilterUtility.getPixel(imageData, i, 0);
+            red += FilterUtility.getPixel(imageData, i + 1, 0);
+            red += FilterUtility.getPixel(imageData, i + imgWidth - 1, 0);
+            red += FilterUtility.getPixel(imageData, i + imgWidth, 0);
+            red += FilterUtility.getPixel(imageData, i + imgWidth + 1, 0);
+
+            green = FilterUtility.getPixel(imageData, i - imgWidth - 1, 1);
+            green += FilterUtility.getPixel(imageData, i - imgWidth, 1);
+            green += FilterUtility.getPixel(imageData, i - imgWidth + 1, 1);
+            green += FilterUtility.getPixel(imageData, i - 1, 1);
+            green += FilterUtility.getPixel(imageData, i, 1);
+            green += FilterUtility.getPixel(imageData, i + 1, 1);
+            green += FilterUtility.getPixel(imageData, i + imgWidth - 1, 1);
+            green += FilterUtility.getPixel(imageData, i + imgWidth, 1);
+            green += FilterUtility.getPixel(imageData, i + imgWidth + 1, 1);
+
+            blue = FilterUtility.getPixel(imageData, i - imgWidth - 1, 2);
+            blue += FilterUtility.getPixel(imageData, i - imgWidth, 2);
+            blue += FilterUtility.getPixel(imageData, i - imgWidth + 1, 2);
+            blue += FilterUtility.getPixel(imageData, i - 1, 2);
+            blue += FilterUtility.getPixel(imageData, i, 2);
+            blue += FilterUtility.getPixel(imageData, i + 1, 2);
+            blue += FilterUtility.getPixel(imageData, i + imgWidth - 1, 2);
+            blue += FilterUtility.getPixel(imageData, i + imgWidth, 2);
+            blue += FilterUtility.getPixel(imageData, i + imgWidth + 1, 2);
+
+            medianImage[i] = alpha << 24 | (int) red << 16 | (int) green << 8 | (int) blue;
+        }
     }
 }
+
--- src/filters/Sharpen.java
+++ src/filters/Sharpen.java
@@ -33,35 +33,35 @@ public class Sharpen {
 
             alpha = (imageData[i] >> 24) & 0xff;
 
-            red = (FilterUtility.getPixel(imageData, i - imgWidth - 1, 0)) * sharpKernel[0];
-            red += (FilterUtility.getPixel(imageData, i - imgWidth, 0)) * sharpKernel[1];
-            red += (FilterUtility.getPixel(imageData, i - imgWidth + 1, 0)) * sharpKernel[2];
-            red += (FilterUtility.getPixel(imageData, i - 1, 0)) * sharpKernel[3];
+            red = FilterUtility.getPixel(imageData, i - imgWidth - 1, 0) * sharpKernel[0];
+            red += FilterUtility.getPixel(imageData, i - imgWidth, 0) * sharpKernel[1];
+            red += FilterUtility.getPixel(imageData, i - imgWidth + 1, 0) * sharpKernel[2];
+            red += FilterUtility.getPixel(imageData, i - 1, 0) * sharpKernel[3];
             red += FilterUtility.getPixel(imageData, i, 0) * sharpKernel[4];
-            red += (FilterUtility.getPixel(imageData, i + 1, 0)) * sharpKernel[5];
-            red += (FilterUtility.getPixel(imageData, i + imgWidth - 1, 0)) * sharpKernel[6];
-            red += (FilterUtility.getPixel(imageData, i + imgWidth, 0)) * sharpKernel[7];
-            red += (FilterUtility.getPixel(imageData, i + imgWidth + 1, 0)) * sharpKernel[8];
+            red += FilterUtility.getPixel(imageData, i + 1, 0) * sharpKernel[5];
+            red += FilterUtility.getPixel(imageData, i + imgWidth - 1, 0) * sharpKernel[6];
+            red += FilterUtility.getPixel(imageData, i + imgWidth, 0) * sharpKernel[7];
+            red += FilterUtility.getPixel(imageData, i + imgWidth + 1, 0) * sharpKernel[8];
 
-            green = (FilterUtility.getPixel(imageData, i - imgWidth - 1, 1)) * sharpKernel[0];
-            green += (FilterUtility.getPixel(imageData, i - imgWidth, 1)) * sharpKernel[1];
-            green += (FilterUtility.getPixel(imageData, i - imgWidth + 1, 1)) * sharpKernel[2];
-            green += (FilterUtility.getPixel(imageData, i - 1, 1)) * sharpKernel[3];
+            green = FilterUtility.getPixel(imageData, i - imgWidth - 1, 1) * sharpKernel[0];
+            green += FilterUtility.getPixel(imageData, i - imgWidth, 1) * sharpKernel[1];
+            green += FilterUtility.getPixel(imageData, i - imgWidth + 1, 1) * sharpKernel[2];
+            green += FilterUtility.getPixel(imageData, i - 1, 1) * sharpKernel[3];
             green += FilterUtility.getPixel(imageData, i, 1) * sharpKernel[4];
-            green += (FilterUtility.getPixel(imageData, i + 1, 1)) * sharpKernel[5];
-            green += (FilterUtility.getPixel(imageData, i + imgWidth - 1, 1)) * sharpKernel[6];
-            green += (FilterUtility.getPixel(imageData, i + imgWidth, 1)) * sharpKernel[7];
-            green += (FilterUtility.getPixel(imageData, i + imgWidth + 1, 1)) * sharpKernel[8];
+            green += FilterUtility.getPixel(imageData, i + 1, 1) * sharpKernel[5];
+            green += FilterUtility.getPixel(imageData, i + imgWidth - 1, 1) * sharpKernel[6];
+            green += FilterUtility.getPixel(imageData, i + imgWidth, 1) * sharpKernel[7];
+            green += FilterUtility.getPixel(imageData, i + imgWidth + 1, 1) * sharpKernel[8];
 
-            blue = (FilterUtility.getPixel(imageData, i - imgWidth - 1, 2)) * sharpKernel[0];
-            blue += (FilterUtility.getPixel(imageData, i - imgWidth, 2)) * sharpKernel[1];
-            blue += (FilterUtility.getPixel(imageData, i - imgWidth + 1, 2)) * sharpKernel[2];
-            blue += (FilterUtility.getPixel(imageData, i - 1, 2)) * sharpKernel[3];
+            blue = FilterUtility.getPixel(imageData, i - imgWidth - 1, 2) * sharpKernel[0];
+            blue += FilterUtility.getPixel(imageData, i - imgWidth, 2) * sharpKernel[1];
+            blue += FilterUtility.getPixel(imageData, i - imgWidth + 1, 2) * sharpKernel[2];
+            blue += FilterUtility.getPixel(imageData, i - 1, 2) * sharpKernel[3];
             blue += FilterUtility.getPixel(imageData, i, 2) * sharpKernel[4];
-            blue += (FilterUtility.getPixel(imageData, i + 1, 2)) * sharpKernel[5];
-            blue += (FilterUtility.getPixel(imageData, i + imgWidth - 1, 2)) * sharpKernel[6];
-            blue += (FilterUtility.getPixel(imageData, i + imgWidth, 2)) * sharpKernel[7];
-            blue += (FilterUtility.getPixel(imageData, i + imgWidth + 1, 2)) * sharpKernel[8];
+            blue += FilterUtility.getPixel(imageData, i + 1, 2) * sharpKernel[5];
+            blue += FilterUtility.getPixel(imageData, i + imgWidth - 1, 2) * sharpKernel[6];
+            blue += FilterUtility.getPixel(imageData, i + imgWidth, 2) * sharpKernel[7];
+            blue += FilterUtility.getPixel(imageData, i + imgWidth + 1, 2) * sharpKernel[8];
 
             if (red < 0) red = 0;
             if (green < 0) green = 0;
